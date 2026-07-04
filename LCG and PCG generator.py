824597e8791d3bf4f918e8bd10b7e7ca4c999abf
ZYR0PH4G3 <shukla.d3stry0.arnav@gmail.com>
2026-07-04 14:47:22 +0530

    Made code for qcg

--- LCG and PCG generator.py
+++ LCG and PCG generator.py
@@ -47,6 +47,30 @@ class PCGPseudoRandomGenerator:
              scaled = (pcg_output / 0xFFFFFFFF) * (num_range[1] - num_range[0]) + num_range[0]
              return int(scaled)   
     
+#QCG Code
+class QCGPseudoRandomGenerator:
+    def __init__(self, a=17, b=3, c=7, m=2**31-1, seed=None):
+
+        self.a=a
+        self.b=b
+        self.c=c
+        self.m=m
+
+        if seed is None:
+            self.x0=int(os.getpid()+time.time())
+        else: 
+            self.x0=seed
+        self.x_prev=(self.a*(self.x0**2)+self.b*self.x0+self.c)%self.m
+    
+    def generate_number(self, num_range=None):
+        self.x_prev=(self.a*(self.x_prev**2)+self.b*self.x_prev+self.c)%self.m
+
+        if num_range is None:
+            return self.x_prev
+        else:
+            scaled = (self.x_prev/(self.m-1))*(num_range[1]-num_range[0])+num_range[0]
+            return int(scaled)    
+
 lcg = LCGPseudoRandomGenerator()
 print("Printing LCG values")
 print(lcg.generate_number([0,100]))
@@ -62,3 +86,11 @@ print(pcg.generate_number([0,100]))
 print(pcg.generate_number([0,100]))
 print(pcg.generate_number([0,100]))
 print(pcg.generate_number([0,100]))
+
+qcg = QCGPseudoRandomGenerator()
+print("Printing QCG values")
+print(qcg.generate_number([0,100]))
+print(qcg.generate_number([0,100]))
+print(qcg.generate_number([0,100]))
+print(qcg.generate_number([0,100]))
+print(qcg.generate_number([0,100]))
