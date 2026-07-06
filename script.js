265428e7bcd1a3e6e115ef3fa3020793550a80bd
ZYR0PH4G3 <shukla.d3stry0.arnav@gmail.com>
2026-07-06 23:32:05 +0530

    finished the dice roller website by adding my own lcg

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
+
+class LCGPseudoRandomGenerator {
+    constructor(seed = null) {
+        this.a = 48271;
+        this.c = 0;
+        this.m = 2147483647; // 2^31 - 1
+
+        if (seed === null || seed === undefined) {
+            this.x0 = Math.floor(Date.now());
+        } else {
+            this.x0 = seed;
+        }
+
+        this.x_prev = (this.a * this.x0 + this.c) % this.m;
+    }
+
+    generateNumber(numRange = null) {
+        this.x_prev = (this.a * this.x_prev + this.c) % this.m;
+
+        if (numRange === null) {
+            return this.x_prev;
+        } else {
+            const scaled = (this.x_prev / (this.m - 1)) * (numRange[1] - numRange[0]) + numRange[0];
+            return Math.floor(scaled); 
+        }
+    }
+}
+
+const lcg = new LCGPseudoRandomGenerator();
+
 function createDice(number) {
     const dotPositionMatrix = {
-        1: [
-            [50,50]
-        ],
-        2: [
-            [20, 20],
-            [80, 80]
-        ],
-        3: [
-            [20, 20],
-            [50, 50],
-            [80, 80],
-        ],
-        4: [
-            [20, 20],
-            [20, 80],
-            [80, 20],
-            [80, 80],
-        ],
-        5: [
-            [20, 20],
-            [20, 80],
-            [50, 50],
-            [80, 20],
-            [80, 80],
-        ],
-        6: [
-            [20, 20],
-            [20, 80],
-            [50, 20],
-            [50, 80],
-            [80, 20],
-            [80, 80],
-        ]
+        1: [[50, 50]],
+        2: [[20, 20],[80, 80]],
+        3: [[20, 20], [50, 50], [80, 80]],
+        4: [[20, 20], [20, 80], [80, 20], [80, 80]],
+        5: [[20, 20], [20, 80], [50, 50], [80, 20], [80, 80]],
+        6: [[20, 20], [20, 80], [50, 20], [50, 80], [80, 20], [80, 80]]
     };
 
     const dice = document.createElement("div");
-
     dice.classList.add("dice");
 
     for (const dotPosition of dotPositionMatrix[number]) {
         const dot = document.createElement("div");
-
         dot.classList.add("dice-dot");
         dot.style.setProperty("--top", dotPosition[0] + "%");
         dot.style.setProperty("--left", dotPosition[1] + "%");
@@ -52,26 +53,28 @@ function createDice(number) {
 }
 
 function randomizeDice(diceContainer, numberOfDice) {
-    diceContainer.innerHTML  = "";
+    diceContainer.innerHTML = "";
 
     for (let i = 0; i < numberOfDice; i++) {
-        const random = Math.floor((Math.random() *6) + 1);
+        const random = lcg.generateNumber([1, 6]);
         const dice = createDice(random);
-
         diceContainer.appendChild(dice);
     }
 }
 
-const NUMBER_OF_DICE = 5;
-const diceContainer = document.querySelector(".dice-container");
-const btnRollDice = document.querySelector(".btn-roll-dice");
+document.addEventListener("DOMContentLoaded", () => {
+    const NUMBER_OF_DICE = 5;
+    const diceContainer = document.querySelector(".dice-container");
+    const btnRollDice = document.querySelector(".btn-roll-dice");
 
-randomizeDice(diceContainer, NUMBER_OF_DICE);
+    randomizeDice(diceContainer, NUMBER_OF_DICE);
 
-btnRollDice.addEventListener("click", () => {
-    const interval = setInterval(() => {
-        randomizeDice(diceContainer, NUMBER_OF_DICE);
-    }, 50);
+    btnRollDice.addEventListener("click", () => {
+        const interval = setInterval(() => {
+            randomizeDice(diceContainer, NUMBER_OF_DICE);
+        }, 50);
 
-    setTimeout(() => clearInterval(interval), 1000);
+        setTimeout(() => clearInterval(interval), 1000);
+    });
 });
+
